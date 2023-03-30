8786b351f025a87627eb53bb0727748fd96f3f93
breval <breval2000@live.fr>
2023-03-31 00:13:28 +0200

    ✨ fonctionnalité(makeup-artiste.js) : ajouter un contrôleur pour makeup-artiste Le contrôleur pour makeup-artiste a été ajouté pour gérer les requêtes entrantes pour cette ressource. Le contrôleur utilise la méthode createCoreController de Strapi pour créer un contrôleur de base pour la ressource makeup-artiste.

---
+++ src/api/makeup-artiste/routes/makeup-artiste.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * makeup-artiste router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::makeup-artiste.makeup-artiste');
---
+++ src/api/makeup-artiste/services/makeup-artiste.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * makeup-artiste service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::makeup-artiste.makeup-artiste');
