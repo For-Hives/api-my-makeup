316c2650e0a9904551b6f11292b89759f10d8e02
brlef <breval.lefloch@rca.fr>
2023-03-31 16:19:05 +0200

    🔧 chore(middlewares.js): add S3 url to content security policy 🔧 chore(plugins.js): add minio-for-strapi-v4 plugin to package.json and configure upload provider with S3 credentials

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,39 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = ({ env }) => {
+  var ssl = env("S3_SSL", "false") === "true" ? "https://" : "http://";
+  const s3url = new URL(ssl + env("S3_ENDPOINT") + ":" + env("S3_PORT"));
+  return [
+    'strapi::errors',
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            "connect-src": ["'self'", "https:", "http:"],
+            "img-src": [
+              "'self'",
+              "data:",
+              "blob:",
+              s3url.host
+            ],
+            "media-src": [
+              "'self'",
+              "data:",
+              "blob:",
+              s3url.host
+            ],
+            upgradeInsecureRequests: null
+          }
+        }
+      }
+    },
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public'
+  ]
+};
---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'minio-for-strapi-v4',
+      providerOptions: {
+        endPoint: env("S3_ENDPOINT"), //s3.example.com
+        port: parseInt(env("S3_PORT"), 9000), //9000
+        useSSL: env("S3_SSL", false) === "true", //true or false
+        accessKey: env("S3_ACCESS_KEY_ID"),
+        secretKey: env("S3_ACCESS_SECRET"),
+        bucket: env("S3_BUCKET")
+      }
+    }
+  }
+
+  // ...
+});
