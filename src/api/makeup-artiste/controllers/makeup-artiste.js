724231e8b33e8d537c10d90e4c56983bc62a5652
Breval Le Floch <breval2000@live.fr>
2023-04-12 18:11:19 +0200

    Dev/breval/structure (#17)
    * ✨ fonctionnalité(makeup-artiste.js) : ajouter un contrôleur pour makeup-artiste
    Le contrôleur pour makeup-artiste a été ajouté pour gérer les requêtes entrantes pour cette ressource. Le contrôleur utilise la méthode createCoreController de Strapi pour créer un contrôleur de base pour la ressource makeup-artiste.
    
    * ✨ fonctionnalité(makeupartistskills) : ajouter les fichiers JSON pour les compétences et les réseaux sociaux
    Les fichiers JSON ajoutent des modèles de données pour les compétences et les réseaux sociaux des artistes de maquillage. Cela permettra de stocker et de récupérer ces informations à partir de la base de données.
    
    * ✨ fonctionnalité(schema.json) : ajouter un schéma pour la collection de types de contenu makeup_artistes
    Ajout d'un schéma pour la collection de types de contenu makeup_artistes. Le schéma définit les attributs pour les makeup_artistes, y compris leur nom, prénom, spécialité, ville, rayon d'action, score, frais de kilométrage, disponibilité, compétences, description, réseau et langue. Cela permettra de mieux organiser les données et de faciliter la recherche et la récupération des informations sur les makeup_artistes.
    
    * 🚀 chore(package.json): update Strapi dependencies to version 4.9.0
    🆕 feat(makeup-artiste): add new fields to the makeup-artiste content type schema
    🆕 feat(makeupartists): add new components for makeupartists content type
    🆕 feat(service-offers): add new component for service-offers content type
    🆕 feat(users-permissions): add new field to the user content type schema
    
    * 🎨 style(favicon, schema.json, service-offers.json) : ajouter des fichiers d'icône et de galerie d'images et mettre à jour les schémas de données
    Les fichiers favicon.ico et favicon.webp ont été ajoutés pour améliorer l'apparence de l'application. Le schéma de données makeup-artiste a été mis à jour pour inclure une galerie d'images pour chaque artiste de maquillage. Le schéma de données service-offers a également été mis à jour pour inclure une image principale pour chaque offre de service.
    
    * 🔧 refactor(build-only.yml) : renommer le travail de "build-and-push" à "just-build"
    Le travail ne pousse plus l'image construite vers un registre, il ne fait que construire l'image. Le nouveau nom reflète mieux ce que fait le travail.
    
    ---------
    
    Co-authored-by: brlef <breval.lefloch@rca.fr>

---
+++ src/api/makeup-artiste/controllers/makeup-artiste.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * makeup-artiste controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::makeup-artiste.makeup-artiste');
