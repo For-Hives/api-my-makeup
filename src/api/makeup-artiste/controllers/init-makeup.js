7e62a96925a37b97d4e6b0da03a297141d5bf2e2
Breval Le Floch <breval2000@live.fr>
2023-04-17 21:51:54 +0200

    ✨ feat(init-makeup): add init-makeup endpoint to create a new makeup … (#27)
    * ✨ feat(init-makeup): add init-makeup endpoint to create a new makeup artist for user id.
    
    * 🐛 fix(init-makeup.js): pass user object to createMakeupArtist function
    ✨ feat(init-makeup.js): add console.log to debug user object
    ✨ feat(init-makeup.js): create new makeup artist for user object in createMakeupArtist function
    
    * 🐛 fix(init-makeup.js): change test variable to json variable
    🐛 fix(init-makeup.js): change error message to 'Init Makeup artist controller error'
    🐛 fix(init-makeup.js): log error body
    ✨ feat(init-makeup.js): add check for existing makeup artist for user
    ✨ feat(init-makeup.js): throw error if user not found
    ✨ feat(init-makeup.js): throw error if makeup artist already exists for user
    ✨ feat(init-makeup.js): create new makeup artist for user id
    
    * 🐛 corriger(init-makeup.js) : supprimer les espaces inutiles dans la déclaration de la variable json
    🔥 suppression(init-makeup.js) : supprimer le paramètre test inutilisé de la fonction createMakeupArtist
    ✨ fonctionnalité(init-makeup.js) : ajouter la création d'un nouvel artiste de maquillage lors de l'initialisation
    La déclaration de la variable json contenait des espaces inutiles qui ont été supprimés pour améliorer la lisibilité du code. Le paramètre test inutilisé a été supprimé de la fonction createMakeupArtist. La fonction createMakeupArtist a été modifiée pour créer un nouvel artiste de maquillage lors de l'initialisation.
    
    * 🐛 corriger(schema.json) : rendre les champs last_name et first_name non obligatoires
    🐛 corriger(schema.json) : rendre le champ main_picture non obligatoire
    🐛 corriger(full_documentation.json) : mettre à jour la date de génération
    Les champs last_name et first_name ne sont plus obligatoires, car ils ne sont pas toujours pertinents pour les artistes de maquillage. Le champ main_picture n'est plus obligatoire car il n'est pas toujours possible d'avoir une image pour chaque artiste. La date de génération de la documentation a été mise à jour pour refléter la dernière version.
    
    * 🐛 corriger(init-makeup.js) : supprimer les lignes inutiles
    Les lignes "first_name: "test", last_name: "test"" ne sont pas nécessaires et ne sont pas utilisées dans le code.
    
    ---------

---
+++ src/api/makeup-artiste/controllers/init-makeup.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * A set of functions called "actions" for `init-makeup`
+ */
+
+module.exports = {
+  async initMakeup(ctx, next) {
+    try {
+
+      const {json} = ctx.request.body;
+
+      const data = await strapi
+        .service("api::makeup-artiste.init-makeup")
+        .createMakeupArtist(ctx.state.user, json);
+
+      ctx.body = data;
+
+    } catch (err) {
+      console.log(err)
+      ctx.badRequest("Init Makeup artist controller error", {moreDetails: err});
+    }
+  }
+};
