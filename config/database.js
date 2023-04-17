84661a44f9d23060c4aeafbaf25a947ca7a574bd
brlef <breval.lefloch@rca.fr>
2023-04-17 10:25:09 +0200

    🐛 fix(database.js): add pool configuration to database configuration 📝 docs(full_documentation.json): update x-generation-date to 2023-04-17T08:23:14.750Z

--- config/database.js
+++ config/database.js
@@ -9,8 +9,19 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'bank'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '0000'),
+      password: env('DATABASE_PASSWORD', '0000')
     },
     useNullAsDefault: true,
-  },
+    pool: {
+      min: 0,
+      max: 100,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 50000,
+      idleTimeoutMillis: 300000,
+      reapIntervalMillis: 10000,
+      createRetryIntervalMillis: 2000,
+      propagateCreateError: false
+    }
+  }
 });
