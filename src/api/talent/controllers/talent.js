3bf0025c30a8bae23316d9fbc5784d0bd23668c4
Cinquin Andy <55706543+CinquinAndy@users.noreply.github.com>
2023-06-16 19:55:30 +0200

    feat(#87): Créer la partie "Talents" pour gérer les spécialités (seo) (#88)

---
+++ src/api/talent/controllers/talent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * talent controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::talent.talent');
